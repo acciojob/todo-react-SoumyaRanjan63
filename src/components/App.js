7fd8d7eff70ba9cf0e7c63e4de9bf8be5e3043c7
SoumyaRanjan63 <sranjan6370@gmail.com>
2023-09-16 00:33:14 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,55 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todolist, setTodolist] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
+
+  const addTodo = () => {
+    if (taskInput.trim() !== "") {
+      // Add the task to the todolist array
+      setTodolist([...todolist, taskInput]);
+
+      // Clear the input field
+      setTaskInput("");
+    }
+  };
+
+  const handleInputChange = (event) => {
+    // Update the taskInput state when the input value changes
+    setTaskInput(event.target.value);
+  };
+
+  const deleteTodo = (index) => {
+    // Create a copy of the todolist without the task at the specified index
+    const updatedTodolist = [...todolist.slice(0, index), ...todolist.slice(index + 1)];
+    setTodolist(updatedTodolist);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <h1>To-Do-List</h1>
+      <input
+        type="text"
+        className="text-inpt"
+        placeholder="add item"
+        value={taskInput}
+        onChange={handleInputChange}
+      />
+      <button type="button" onClick={addTodo}>
+        Add Todo
+      </button>
 
-export default App
+      <ul>
+        {todolist.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
